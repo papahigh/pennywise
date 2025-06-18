79e06eaaca6c15b3f50fb82d765d59fde6bde901
Nikola P <nikolay.papakha@gmail.com>
2025-06-18 12:38:51 +0300

    add android theme

--- composeApp/src/androidMain/kotlin/io/papahgh/pennywise/App.kt
+++ composeApp/src/androidMain/kotlin/io/papahgh/pennywise/App.kt
@@ -7,21 +7,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import io.papahgh.pennywise.ui.theme.PennywiseTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import pennywise.composeapp.generated.resources.Res
 import pennywise.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    PennywiseTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
---
+++ composeApp/src/androidMain/kotlin/io/papahgh/pennywise/ui/theme/Type.kt
@@ -0,0 +1,43 @@
+package io.papahgh.pennywise.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Normal
+import org.jetbrains.compose.resources.Font
+import pennywise.composeapp.generated.resources.Res
+import pennywise.composeapp.generated.resources.libre_baskerville_bold
+import pennywise.composeapp.generated.resources.libre_baskerville_italic
+import pennywise.composeapp.generated.resources.libre_baskerville_regular
+
+
+@Composable
+fun displayFontFamily() = FontFamily(
+    Font(Res.font.libre_baskerville_regular, Normal),
+    Font(Res.font.libre_baskerville_italic, Normal, FontStyle.Italic),
+    Font(Res.font.libre_baskerville_bold, FontWeight.Bold)
+)
+
+@Composable
+fun pennywiseTypography() = Typography().run {
+    val displayFontFamily = displayFontFamily()
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = displayFontFamily),
+        displayMedium = displayMedium.copy(fontFamily = displayFontFamily),
+        displaySmall = displaySmall.copy(fontFamily = displayFontFamily),
+        headlineLarge = headlineLarge.copy(fontFamily = displayFontFamily),
+        headlineMedium = headlineMedium.copy(fontFamily = displayFontFamily),
+        headlineSmall = headlineSmall.copy(fontFamily = displayFontFamily),
+        titleLarge = titleLarge.copy(fontFamily = displayFontFamily),
+        titleMedium = titleMedium.copy(fontFamily = displayFontFamily),
+        titleSmall = titleSmall.copy(fontFamily = displayFontFamily),
+        bodyLarge = bodyLarge.copy(),
+        bodyMedium = bodyMedium.copy(),
+        bodySmall = bodySmall.copy(),
+        labelLarge = labelLarge.copy(),
+        labelMedium = labelMedium.copy(),
+        labelSmall = labelSmall.copy(),
+    )
+}
