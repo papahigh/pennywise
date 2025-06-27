d49c41aa289d2aa1b725bc58b0ffa42aed29149c
Nikola P <nikolay.papakha@gmail.com>
2025-06-27 16:10:34 +0300

    fix TopAppBar

--- androidApp/src/main/java/io/papahigh/pennywise/components/TopAppBar.kt
+++ androidApp/src/main/java/io/papahigh/pennywise/components/TopAppBar.kt
@@ -1,21 +1,21 @@
 package io.papahigh.pennywise.components
 
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import io.papahigh.pennywise.R
+import androidx.compose.material3.TopAppBar as MaterialTopAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(title: String = stringResource(R.string.app_name)) {
-    CenterAlignedTopAppBar(
+fun TopAppBar(title: String = stringResource(R.string.appName)) {
+    MaterialTopAppBar(
         title = {
             Text(
                 text = title,
-                style = MaterialTheme.typography.headlineMedium,
+                style = MaterialTheme.typography.displaySmall,
             )
         },
     )
